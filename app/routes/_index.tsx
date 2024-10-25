f975fcfb8ed4859ebfb42a00f7b2a71986accfcb
jimao <shihao.gu@perfma.com>
2024-10-25 17:14:54 +0800

    简单的折线图

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,7 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
 import { Header } from "~/components/Header";
+import { createChart, IChartApi } from 'lightweight-charts';
+import { useEffect, useRef } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,7 +11,30 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  let chart = useRef<IChartApi>();
+  const chartDOM = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!chart.current) {
+      chart.current = createChart(chartDOM.current!, { height: 300 });
+      const lineSeries = chart.current.addLineSeries();
+      lineSeries.setData([
+        { time: '2019-04-11', value: 80.01 },
+        { time: '2019-04-12', value: 96.63 },
+        { time: '2019-04-13', value: 76.64 },
+        { time: '2019-04-14', value: 81.89 },
+        { time: '2019-04-15', value: 74.43 },
+        { time: '2019-04-16', value: 80.01 },
+        { time: '2019-04-17', value: 96.63 },
+        { time: '2019-04-18', value: 76.64 },
+        { time: '2019-04-19', value: 81.89 },
+        { time: '2019-04-20', value: 74.43 },
+      ]);
+    }
+  }, []);
+
   return <div>
     <Header />
+    <div ref={chartDOM}></div>
   </div>;
 }
